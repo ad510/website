0815277e86b6fe09ce22e8f870893e4eeb998ce3
Andrew Downing <ad510n1@gmail.com>
2015-02-27 23:35:40 -0800

    titles

--- src/gen.clj
+++ src/gen.clj
@@ -1,13 +1,15 @@
-(ns gen (:require [net.cgrand.enlive-html :as html]
-                  [me.raynes.fs :as fs]))
+(ns gen (:require clojure.edn
+                  [me.raynes.fs :as fs])
+        (:use net.cgrand.enlive-html))
 
-(html/deftemplate base "base.htm" [url title]
-  [:title] (html/content title)
-  [[:a (html/attr= :href url)]] (html/add-class "sel")
-  [:#pgcontent] (html/html-content (slurp (str "src/pg/" url))))
+(deftemplate base "base.htm" [url title]
+  [:title] (content title)
+  [[:a (attr= :href url)]] (add-class "sel")
+  [:#pgcontent] (html-content (slurp (str "src/pg/" url))))
 
 (defn -main []
   (fs/delete-dir "src/out")
   (fs/copy-dir "src/static" "src/out")
-  (doseq [f (.list (fs/file "src/pg"))]
-    (spit (str "src/out/" f) (apply str (base f "my title")))))
+  (let [titles (clojure.edn/read-string (slurp "src/titles.txt"))]
+    (doseq [f (.list (fs/file "src/pg"))]
+      (spit (str "src/out/" f) (apply str (base f (get titles f)))))))

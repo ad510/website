12df191c0e95b3ecd25d78f7b58801a69ca9f9cc
Andrew Downing <ad510n1@gmail.com>
2015-06-08 21:05:59 -0700

    submenus

--- src/gen.clj
+++ src/gen.clj
@@ -2,14 +2,15 @@
                   [me.raynes.fs :as fs])
         (:use net.cgrand.enlive-html))
 
-(deftemplate base "base.htm" [url title]
-  [:title] (content title)
+(deftemplate base "base.htm" [url cfg]
+  [:title] (content (cfg :title))
   [[:a (attr= :href url)]] (add-class "sel")
+  [[:.submenu (but (id= (cfg :submenu)))]] nil
   [:#pgcontent] (html-content (slurp (str "src/pg/" url))))
 
 (defn -main []
   (fs/delete-dir "src/out")
   (fs/copy-dir "src/static" "src/out")
-  (let [titles (clojure.edn/read-string (slurp "src/titles.txt"))]
+  (let [cfg (clojure.edn/read-string (slurp "src/cfg.txt"))]
     (doseq [f (.list (fs/file "src/pg"))]
-      (spit (str "src/out/" f) (apply str (base f (get titles f)))))))
+      (spit (str "src/out/" f) (apply str (base f (cfg f)))))))

d7c3c319d510e7925ba85f3b8517229c5133f91d
Andrew Downing <ad510n1@gmail.com>
2015-07-11 15:32:26 -0700

    import content

---
+++ src/in/boredgamejam/common.js
@@ -0,0 +1,50 @@
+function getMousePos(e) // thanks to quirksmode.org/js/events_properties.html
+{
+	if (!e) var e = window.event;
+	if (e.pageX || e.pageY) {
+		xMouse = e.pageX;
+		yMouse = e.pageY;
+	}
+	else if (e.clientX || e.clientY) {
+		xMouse = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+		yMouse = (e.clientY + document.body.scrollTop + document.documentElement.scrollTop);
+	}
+}
+
+function getWindowWidth() // based on http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
+{
+	if (typeof(window.innerWidth) == 'number')
+	{
+		// Non-IE
+		return window.innerWidth;
+	}
+	else if (document.documentElement && document.documentElement.clientWidth)
+	{
+		// IE 6+ in 'standards compliant mode'
+		return document.documentElement.clientWidth;
+	}
+}
+
+function getWindowHeight() // based on http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
+{
+	if (typeof(window.innerHeight) == 'number')
+	{
+		// Non-IE
+		return window.innerHeight;
+	}
+	else if (document.documentElement && document.documentElement.clientHeight)
+	{
+		// IE 6+ in 'standards compliant mode'
+		return document.documentElement.clientHeight;
+	}
+}
+
+// based on http://quirksmode.org/js/events_properties.html
+function findKey(e)
+{
+	var code;
+	if (!e) var e = window.event;
+	if (e.keyCode) code = e.keyCode;
+	else if (e.which) code = e.which;
+	return String.fromCharCode(code);
+}

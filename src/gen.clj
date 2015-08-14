de68d68116f4cd544f4b6013db62466ce5a8f3b8
Andrew Downing <ad510n1@gmail.com>
2015-08-14 14:01:19 -0700

    move content out of src

--- src/gen.clj
+++ src/gen.clj
@@ -7,12 +7,12 @@
   [:title] (content ((pg 1) :title))
   [[:a (attr= :href (str (pg 0) ".htm"))]] (add-class "sel")
   [[:.submenu (but (id= ((pg 1) :submenu)))]] nil
-  [:#pgcontent] (html-content (slurp (str "src/in/" (pg 0) ".htm"))))
+  [:#pgcontent] (html-content (slurp (str "in/" (pg 0) ".htm"))))
 
 (defn -main []
-  (fs/delete-dir "src/out")
-  (fs/copy-dir "src/in" "src/out")
-  (spit "src/out/emai1.txt" (clojure.string/join (interpose "," (reverse
-    (map-indexed (fn [i c] (mod (- (int c) (* i 42) 1) 256)) (slurp "src/emai1.txt"))))))
-  (doseq [pg (clojure.edn/read-string (slurp "src/pgs.txt"))]
-    (spit (str "src/out/" (pg 0) ".htm") (apply str (base pg)))))
+  (fs/delete-dir "out")
+  (fs/copy-dir "in" "out")
+  (spit "out/emai1.txt" (clojure.string/join (interpose "," (reverse
+    (map-indexed (fn [i c] (mod (- (int c) (* i 42) 1) 256)) (slurp "emai1.txt"))))))
+  (doseq [pg (clojure.edn/read-string (slurp "pgs.txt"))]
+    (spit (str "out/" (pg 0) ".htm") (apply str (base pg)))))

06b57c7ec5f75dce2010be36f70685b711d680fe
Andrew Downing <ad510n1@gmail.com>
2016-11-07 19:57:47 -0800

    rm clojure site generator in favor of gen.py

--- gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ ! -f website-0.1.0-standalone.jar ]; then
-  lein uberjar
-  cp target/website-0.1.0-standalone.jar .
-fi
-java -jar website-0.1.0-standalone.jar
--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject website "0.1.0"
-  :main gen
-  :aot [gen]
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [enlive "1.1.5"]
-                 [me.raynes/fs "1.4.6"]])
